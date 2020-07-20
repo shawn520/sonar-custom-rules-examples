1397bc8e7ccfa6119697ea10d1925d1067876582
shawn <liushawn@outlook.com>
2020-07-20 18:38:15 +0800

    增加规则描述

--- java-custom-rules/src/main/java/org/sonar/samples/java/checks/MyFirstCustomCheck.java
+++ java-custom-rules/src/main/java/org/sonar/samples/java/checks/MyFirstCustomCheck.java
@@ -2,6 +2,7 @@ package org.sonar.samples.java.checks;
 
 import com.google.common.collect.ImmutableList;
 
+import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.semantic.Symbol;
@@ -16,7 +17,12 @@ import java.util.List;
  * @author Shawn
  * @date 2020/7/17
  */
-@Rule(key = "MyFirstCustomRule")
+@Rule(
+    key = "MyFirstCustomRule",
+    name = "Return type and parameter of a method should not be the same",
+    description = "For a method having a single parameter, the types of its return value and its parameter should never be the same.",
+    priority = Priority.CRITICAL,
+    tags = {"bug"})
 public class MyFirstCustomCheck extends IssuableSubscriptionVisitor {
 
     @Override
